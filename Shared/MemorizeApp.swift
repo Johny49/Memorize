6fdb30bf45feecf6b9357e12229303423e4d66f1
Geoffrey Johnson <GeoffJ_1@hotmail.com>
2021-07-10 10:05:54 -0500

    Initial Commit

---
+++ Shared/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Shared
+//
+//  Created by Geoffrey Johnson on 7/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
